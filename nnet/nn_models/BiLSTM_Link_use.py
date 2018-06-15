d6a02134a1a483b03c86c6704e52ece0c3d6feb7
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-06-15 22:13:26 +0100

    traning.py

--- nnet/nn_models/BiLSTM_Link_use.py
+++ nnet/nn_models/BiLSTM_Link_use.py
@@ -179,8 +179,10 @@ class BiLSTMTagger(nn.Module):
         dep_tag_space_spe = self.MLP_spe(self.link_dropout(F.tanh(self.hidden2tag_spe(bf_e)))).view(
             len(sentence[0]) * self.batch_size, -1)
 
+        dep_tag_space_spe_nodrop = self.MLP_spe(F.tanh(self.hidden2tag_spe(bf_e))).view(
+            len(sentence[0]) * self.batch_size, -1)
         #TagProbs = torch.FloatTensor(F.softmax(dep_tag_space, dim=1).view(self.batch_size, len(sentence[0]), -1).cpu().data.numpy()).to(device)
-        LinkProbs = torch.FloatTensor(F.softmax(dep_tag_space_spe, dim=1).view(self.batch_size, len(sentence[0]), -1).cpu().data.numpy()).to(device)
+        LinkProbs = torch.FloatTensor(F.softmax(dep_tag_space_spe_nodrop, dim=1).view(self.batch_size, len(sentence[0]), -1).cpu().data.numpy()).to(device)
 
 
         #TagProbs = F.softmax(dep_tag_space, dim=1).view(self.batch_size, len(sentence[0]), -1)
