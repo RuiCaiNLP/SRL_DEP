7f769aa98e8e90c536cea89b44fb02726a454827
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-06-30 10:05:11 +0100

    traning.py

--- nnet/nn_models/BiLSTM_learn_use.py
+++ nnet/nn_models/BiLSTM_learn_use.py
@@ -301,11 +301,12 @@ class BiLSTMTagger(nn.Module):
         #weight = float(SRLloss.cpu().data.numpy())
         #if weight > 0.1:
         #    weight = 0.1
-        p = nr.rand()
-        if p<0.1:
-            loss = SRLloss + DEPloss +  SPEDEPloss
-        else:
-            loss = SRLloss
+        #p = nr.rand()
+        #if p<0.1:
+        #    loss = SRLloss + DEPloss +  SPEDEPloss
+        #else:
+        #    loss = SRLloss
+        loss = SRLloss + 0.1*DEPloss + 0.1*SPEDEPloss
         return SRLloss, DEPloss, SPEDEPloss, loss, SRLprobs, wrong_l_nums, all_l_nums, wrong_l_nums_spe, all_l_nums_spe
 
     @staticmethod
