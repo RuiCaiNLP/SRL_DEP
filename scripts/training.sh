2bc8bf108f630e8ec46fe7f6b6046199c74284f9
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-07-03 00:23:28 +0100

    traning.py

--- scripts/training.sh
+++ scripts/training.sh
@@ -19,7 +19,7 @@ python -mnnet.run.srl.run \
 --eval-dir ./data/ \
 --epochs 20 \
 --out conll2009_rm0_pl_a.25_sskip_h512_d.0_l4 \
---params-path model81.pkl \
+--params-path model_baseline.pkl \
 --hps "{'id': 1, 'sent_edim': 100, 'sent_hdim': 128, \
 'frame_edim': 128, 'role_edim': 128, 'pos_edim': 16, 'rec_layers': 1, 'gc_layers': 0, \
 'pos': True, 'rm':0, 'alpha': 0.25, \
