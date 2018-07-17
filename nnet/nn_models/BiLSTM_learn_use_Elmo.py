a6935d5531d0726ba8f72d78122d09def0df4a60
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-07-17 21:25:18 +0100

    traning.py

--- nnet/nn_models/BiLSTM_learn_use_Elmo.py
+++ nnet/nn_models/BiLSTM_learn_use_Elmo.py
@@ -56,11 +56,11 @@ class BiLSTMTagger(nn.Module):
         self.word_fixed_embeddings.weight.data.copy_(torch.from_numpy(hps['word_embeddings']))
 
         self.elmo_embeddings_0 = nn.Embedding(vocab_size, 1024)
-        self.elmo_embeddings_0.weight.data.copy_(torch.from_numpy(hps['elmo_embeddings_0']))
+        #self.elmo_embeddings_0.weight.data.copy_(torch.from_numpy(hps['elmo_embeddings_0']))
         self.elmo_embeddings_0.weight.requires_grad_(False)
 
         self.elmo_embeddings_1 = nn.Embedding(vocab_size, 1024)
-        self.elmo_embeddings_1.weight.data.copy_(torch.from_numpy(hps['elmo_embeddings_1']))
+        #self.elmo_embeddings_1.weight.data.copy_(torch.from_numpy(hps['elmo_embeddings_1']))
         self.elmo_embeddings_1.weight.requires_grad_(False)
 
         self.role_embeddings = nn.Embedding(self.tagset_size, role_embedding_dim)
