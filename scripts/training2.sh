4f4b7f0bb6d22bac0a54c1f44892cde52d7b0f93
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-07-17 21:21:06 +0100

    traning.py

--- scripts/training2.sh
+++ scripts/training2.sh
@@ -2,8 +2,8 @@
 
 cd ..
 python -mnnet.run.srl.run \
---train conll2009.train.dep.batch \
---dev conll2009.dev.dep.batch \
+--train conll2009_batch.train.dep_l \
+--test conll2009_batch.dev.dep_l \
 --data_partition dev \
 --batch 30 \
 --freq-voc freq.voc_unk.conll2009 \
@@ -13,16 +13,17 @@ python -mnnet.run.srl.run \
 --frame-voc frames.voc.conll2009 \
 --pos-voc pos.voc.conll2009 \
 --dep-voc dep.voc.conll2009_2 \
---lr-dep-voc LR.dep.voc.conll2009 \
+--specific-dep-voc Specific_Dep.voc \
 --word-embeddings word_embeddings_proper.sskip.conll2009.txt \
 --dbg-print-rate 500 \
 --eval-dir ./data/ \
---epochs 10 \
+--epochs 20 \
 --out conll2009_rm0_pl_a.25_sskip_h512_d.0_l4 \
---params-path Model_Joint_hingeLabel_128.pkl \
+--params-path model_pruned.pkl \
 --hps "{'id': 1, 'sent_edim': 100, 'sent_hdim': 128, \
 'frame_edim': 128, 'role_edim': 128, 'pos_edim': 16, 'rec_layers': 1, 'gc_layers': 0, \
 'pos': True, 'rm':0, 'alpha': 0.25, \
 'p_lemmas':True}"
+
 
 
