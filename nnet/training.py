811f1dc1d521dbdb3e1f82141abf89b53ab76ab6
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-07-27 18:09:03 +0100

    rank

--- nnet/training.py
+++ nnet/training.py
@@ -129,8 +129,8 @@ def train(model, train_set, dev_set, test_set, epochs, converter, dbg_print_rate
 
 
             idx += 1
-            Loss = SRLloss + 0.5/(1 + 0.3*(e - 1))*(DEPloss + SPEDEPloss)
-            Loss.backward()
+
+            loss.backward()
             #clip_grad_norm_(parameters=model.hidden2tag_M.parameters(), max_norm=norm)
             #clip_grad_norm_(parameters=model.hidden2tag_H.parameters(), max_norm=norm)
             clip_grad_value_(parameters=model.parameters(), clip_value=3)
