e153bcf1d0c814eb4457e114a6b5a903c6626179
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-08-05 20:37:21 +0100

    new

--- nnet/run/srl/run.py
+++ nnet/run/srl/run.py
@@ -1,7 +1,7 @@
 from nnet.run.runner import *
 from nnet.ml.voc import *
 from functools import partial
-from nnet.nn_models.Share_wordembeds_Newloss import BiLSTMTagger
+from nnet.nn_models.Share_wordembeds_crossEntropy import BiLSTMTagger
 
 def make_local_voc(labels):
     return {i: label for i, label in enumerate(labels)}
