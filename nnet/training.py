24ca595d598ea95539ab439f27ce4f35b59e7920
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-08-07 22:01:21 +0100

    new

--- nnet/training.py
+++ nnet/training.py
@@ -148,7 +148,7 @@ def train(model, train_set, dev_set, test_set, epochs, converter, dbg_print_rate
             if idx < 1000 and False:
                 DEPloss.backward()
             else:
-                SRLloss.backward()
+                loss.backward()
             #clip_grad_norm_(parameters=model.hidden2tag_M.parameters(), max_norm=norm)
             #clip_grad_norm_(parameters=model.hidden2tag_H.parameters(), max_norm=norm)
             #clip_grad_value_(parameters=model.parameters(), clip_value=3)
