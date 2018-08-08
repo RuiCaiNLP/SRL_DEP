3a4abc580165a6ab4f3adef8b1e2f16dde589c51
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-08-08 23:51:47 +0100

    new

--- scripts/training.sh
+++ scripts/training.sh
@@ -23,7 +23,7 @@ python -mnnet.run.srl.run \
 --epochs 20 \
 --out conll2009_rm0_pl_a.25_sskip_h512_d.0_l4 \
 --params-path model_elmo_128.pkl \
---hps "{'id': 1, 'sent_edim': 100, 'sent_hdim': 128, \
+--hps "{'id': 1, 'sent_edim': 100, 'sent_hdim': 256, \
 'frame_edim': 128, 'role_edim': 128, 'pos_edim': 16, 'rec_layers': 1, 'gc_layers': 0, \
 'pos': True, 'rm':0, 'alpha': 0.25, \
 'p_lemmas':True}"
