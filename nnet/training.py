d043644b4fdf6ca105e81bbe3c3ae5b965ad054e
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-08-09 17:10:22 +0100

    new

--- nnet/training.py
+++ nnet/training.py
@@ -137,12 +137,8 @@ def train(model, train_set, dev_set, test_set, epochs, converter, dbg_print_rate
             idx += 1
             #Final_loss = SRLloss + 0.5/(1 + 0.3 *(e-1)) * (DEPloss + SPEDEPloss)
             if batch_idx < dataset_len * 0.1:
-                log('+DEP')
-                log(batch_idx, dataset_len)
                 Final_loss = SRLloss + 0.5 * (DEPloss + SPEDEPloss)
             else:
-                log('No DEP')
-                log(batch_idx, dataset_len)
                 Final_loss = SRLloss
             Final_loss.backward()
             #clip_grad_norm_(parameters=model.hidden2tag_M.parameters(), max_norm=norm)
