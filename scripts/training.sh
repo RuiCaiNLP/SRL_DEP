7fc3a56272000e5e60f380733742e827a3aea02d
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-08-23 22:04:31 +0100

    new

--- scripts/training.sh
+++ scripts/training.sh
@@ -20,7 +20,7 @@ python -mnnet.run.srl.run \
 --eval-dir ./data/ \
 --epochs 20 \
 --out conll2009_rm0_pl_a.25_sskip_h512_d.0_l4 \
---params-path Less_annotation_512_0.1.pkl \
+--params-path Less_annotation_512_0.2.pkl \
 --hps "{'id': 1, 'sent_edim': 100, 'sent_hdim': 512, \
 'frame_edim': 128, 'role_edim': 128, 'pos_edim': 16, 'rec_layers': 1, 'gc_layers': 0, \
 'pos': True, 'rm':0, 'alpha': 0.25, \
