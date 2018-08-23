d4528d11999ef3589cc6737326659283f92f024a
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-08-23 22:23:59 +0100

    new

--- nnet/training.py
+++ nnet/training.py
@@ -136,7 +136,7 @@ def train(model, train_set, dev_set, test_set, epochs, converter, dbg_print_rate
 
             idx += 1
             #Final_loss = SRLloss + 0.5/(1 + 0.3 *(e-1)) * (DEPloss + SPEDEPloss)
-            if batch_idx < dataset_len * 0.8:
+            if batch_idx < dataset_len * 0.9:
                 Final_loss = SRLloss + 0.5 * (DEPloss + SPEDEPloss)
             else:
                 Final_loss = SRLloss
