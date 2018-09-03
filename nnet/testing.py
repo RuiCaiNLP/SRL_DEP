1c00b5d34786ead7c974b810642d3f22c0c0d470
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-09-03 21:24:42 +0100

    new

--- nnet/testing.py
+++ nnet/testing.py
@@ -16,7 +16,7 @@ class NullTester(object):
     def run(self, *args, **kwargs):
         pass
 
-sigf_file = open('Ours_output', 'wr')
+sigf_file = open('SRLonly_output', 'wr')
 
 
 def make_local_voc(labels):
