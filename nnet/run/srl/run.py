876ee6efb121db7798bc8f91b463c2f0358dfcb2
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-09-17 15:37:23 +0100

    fixed biaffine

--- nnet/run/srl/run.py
+++ nnet/run/srl/run.py
@@ -1,7 +1,7 @@
 from nnet.run.runner import *
 from nnet.ml.voc import *
 from functools import partial
-from nnet.nn_models.share_all_biaffine import BiLSTMTagger
+from nnet.nn_models.share_all_DBA import BiLSTMTagger
 
 
 def make_local_voc(labels):
