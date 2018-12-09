450ad2eb72c155e382920834bf1e42a667c97966
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-12-09 23:24:38 +0000

    full parser

--- nnet/nn_models/full_parser.py
+++ nnet/nn_models/full_parser.py
@@ -42,7 +42,7 @@ class BiLSTMTagger(nn.Module):
 
         batch_size = hps['batch_size']
         lstm_hidden_dim = hps['sent_hdim']
-        sent_embedding_dim_DEP = 2*hps['sent_edim'] + 1*hps['pos_edim']
+        sent_embedding_dim_DEP = 1*hps['sent_edim'] + 1*hps['pos_edim']
         sent_embedding_dim_SRL = 3 * hps['sent_edim'] + 1 * hps['pos_edim'] + 16
         ## for the region mark
         role_embedding_dim = hps['role_edim']
@@ -247,7 +247,7 @@ class BiLSTMTagger(nn.Module):
         fixed_embeds_DEP = self.word_fixed_embeddings(sentence_cat)
         fixed_embeds_DEP = fixed_embeds_DEP.view(self.batch_size, len(sentence[0])+1, self.word_emb_dim)
 
-        embeds_forDEP = torch.cat((embeds_DEP, fixed_embeds_DEP, pos_embeds), 2)
+        embeds_forDEP = torch.cat((embeds_DEP,pos_embeds), 2)
         embeds_forDEP = self.DEP_input_dropout(embeds_forDEP)
 
 
