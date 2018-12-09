3afa648b0cbdd3bfe7bc0edd77ec199f855625a3
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-12-09 17:05:50 +0000

    full parser

--- nnet/nn_models/full_parser.py
+++ nnet/nn_models/full_parser.py
@@ -220,7 +220,7 @@ class BiLSTMTagger(nn.Module):
         exprs = [[self.__getExpr(sentence,  i, j, train)
                   for j in xrange(head.size()[0])]
                 for i in xrange(head.size()[0])]
-        scores = np.array([[get_data(output).numpy()[0, 0] for output in exprsRow] for exprsRow in exprs])
+        scores = np.array([[get_data(output).numpy()[0] for output in exprsRow] for exprsRow in exprs])
         return scores, exprs
 
 
