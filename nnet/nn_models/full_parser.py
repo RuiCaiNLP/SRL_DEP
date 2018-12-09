55e084fe88f30fbdd6f2106206e2bf480fd9d17c
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-12-09 23:14:03 +0000

    full parser

--- nnet/nn_models/full_parser.py
+++ nnet/nn_models/full_parser.py
@@ -237,12 +237,12 @@ class BiLSTMTagger(nn.Module):
         elmo_emb_word = self.elmo_mlp_word(elmo_emb)
         """
         #contruct input for DEP
-        sentence_cat = torch.cat((torch.from_numpy(np.zeros((self.batch_size, 1)).astype('int64')).to(device), sentence), 1)
+        sentence_cat = torch.cat((torch.from_numpy(np.zeros((self.batch_size, 1)).astype('int64'), requires_grad=True).to(device), sentence), 1)
         embeds_DEP = self.word_embeddings_DEP(sentence_cat)
         embeds_DEP = embeds_DEP.view(self.batch_size, len(sentence[0])+1, self.word_emb_dim)
-        pos_tags_cat = torch.cat((torch.from_numpy(np.zeros((self.batch_size, 1)).astype('int64')).to(device), pos_tags), 1)
+        pos_tags_cat = torch.cat((torch.from_numpy(np.zeros((self.batch_size, 1)).astype('int64'), requires_grad=True).to(device), pos_tags), 1)
         pos_embeds = self.pos_embeddings(pos_tags_cat)
-        region_marks_cat = torch.cat((torch.from_numpy(np.zeros((self.batch_size, 1)).astype('int64')).to(device), region_marks), 1)
+        region_marks_cat = torch.cat((torch.from_numpy(np.zeros((self.batch_size, 1)).astype('int64'), requires_grad=True).to(device), region_marks), 1)
         region_marks_embeds = self.region_embeddings(region_marks_cat).view(self.batch_size, len(sentence[0])+1, 16)
         #sharing pretrained word_embeds
         fixed_embeds_DEP = self.word_fixed_embeddings(sentence_cat)
