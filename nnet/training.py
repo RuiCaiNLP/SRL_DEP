6f137d8ec8c7f06543b3b22f4d8c24f471ee0cdc
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-12-09 22:21:47 +0000

    full parser

--- nnet/training.py
+++ nnet/training.py
@@ -144,7 +144,7 @@ def train(model, train_set, dev_set, test_set, epochs, converter, dbg_print_rate
             #else:
             #    Final_loss = SRLloss
 
-            Final_loss = SRLloss + 0.5 * (DEPloss + SPEDEPloss)
+            Final_loss = DEPloss
             Final_loss.backward()
             #clip_grad_norm_(parameters=model.hidden2tag_M.parameters(), max_norm=norm)
             #clip_grad_norm_(parameters=model.hidden2tag_H.parameters(), max_norm=norm)
