1240226b8c870443fe27e7afde17efecd59f8d5f
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-12-18 23:37:24 +0000

    separately

--- nnet/nn_models/Seperate_Baseline.py
+++ nnet/nn_models/Seperate_Baseline.py
@@ -260,7 +260,7 @@ class BiLSTMTagger(nn.Module):
         sent_pred_lemmas_embeds = self.p_lemma_embeddings(sent_pred_lemmas_idx)
         embeds_SRL = self.word_embeddings_SRL(sentence)
         embeds_SRL = embeds_SRL.view(self.batch_size, len(sentence[0]), self.word_emb_dim)
-
+        pos_embeds = self.pos_embeddings(pos_tags)
 
         SRL_hidden_states = torch.cat((embeds_SRL,  fixed_embeds, sent_pred_lemmas_embeds, pos_embeds, region_marks,
                                        h1, h2, SRL_composer), 2)
