37123c6512266d4c4a9a2e4400c696b28b70203c
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-12-26 16:58:01 +0000

    test

--- nnet/nn_models/SRL_only_1.py
+++ nnet/nn_models/SRL_only_1.py
@@ -195,6 +195,11 @@ class BiLSTMTagger(nn.Module):
         elmo_emb_word = self.elmo_mlp_word(elmo_emb)
         """
         #contruct input for DEP
+        log(sentence)
+        log(region_marks)
+        log(lengths)
+        log(targets)
+        return
 
         embeds_DEP = self.word_embeddings_DEP(sentence)
         add_zero = torch.zeros((self.batch_size, 1, self.word_emb_dim)).to(device)
