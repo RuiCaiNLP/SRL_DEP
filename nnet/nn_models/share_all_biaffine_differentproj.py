7cefdbfc0203fb3a630f77a8147b5811ec8ace06
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-01-01 22:07:14 +0000

    test

--- nnet/nn_models/share_all_biaffine_differentproj.py
+++ nnet/nn_models/share_all_biaffine_differentproj.py
@@ -28,75 +28,33 @@ def cat(l, dimension=-1):
         dimension += len(valid_l[0].size())
     return torch.cat(valid_l, dimension)
 
-class Biaffine(nn.Module):
-    def __init__(self, in1_features, in2_features, out_features, bias=(True, True, False)):
-        super(Biaffine, self).__init__()
-        self.in1_features = in1_features
-        self.in2_features = in2_features
-        self.out_features = out_features
-        self._use_bias = bias
-
-        shape = (in1_features + int(bias[0]),
-                 in2_features + int(bias[1]),
-                 out_features)
-        self.weight = nn.Parameter(torch.Tensor(*shape))
-        if bias[2]:
-            self.bias = nn.Parameter(torch.Tensor(out_features))
+class MLP(nn.Linear):
+    def __init__(self, in_features, out_features, activation=None, dropout=0.0, bias=True):
+        super(MLP, self).__init__(in_features, out_features, bias)
+        if activation is None:
+            self._activate = lambda x: x
         else:
-            # TODO: why not self.bias = None ?
-            self.register_parameter('bias', None)
-        self.reset_parameters()
+            if not callable(activation):
+                raise ValueError("activation must be callable, but got {}".format(type(activation)))
+            self._activate = activation
+        assert dropout == 0 or type(dropout) == float
+        self._dropout_ratio = dropout
+        if dropout > 0:
+            self._dropout = nn.Dropout(p=self._dropout_ratio)
 
-    def reset_parameters(self):
-        # According to https://github.com/tdozat/Parser-v1/blob/Definitely-working/lib/linalg.py# L97
-        # The parameters are init with orthogonal
-        nn.init.orthogonal_(self.weight)
-        if self.bias is not None:
-            # TODO: need to verify the initialization method
-            #stdv = 1. / math.sqrt(self.bias.size(0))
-            #self.bias.data.uniform_(-stdv, stdv)
-            nn.init.constant_(self.bias, 0)
+    def forward(self, x):
+        size = x.size()
+        if len(size) > 2:
+            y = super(MLP, self).forward(
+                x.contiguous().view(-1, size[-1]))
+            y = y.view(size[0:-1] + (-1,))
+        else:
+            y = super(MLP, self).forward(x)
+        if self._dropout_ratio > 0:
+            return self._dropout(self._activate(y))
+        else:
+            return self._activate(y)
 
-    def forward(self, input1, input2):
-        # TODO: Might set dropout for the inputs, refer to
-        # https://github.com/tdozat/Parser-v1/blob/Definitely-working/lib/models/nn.py#L303
-        is_cuda = next(self.parameters()).is_cuda
-        device_id = next(self.parameters()).get_device() if is_cuda else None
-        out_size = self.out_features
-        batch_size, len1, dim1 = input1.size()
-        if self._use_bias[0]:
-            ones = torch.ones(batch_size, len1, 1)
-            if is_cuda:
-                ones = ones.cuda(device_id)
-            input1 = torch.cat([input1, Variable(ones)], dim=2)
-            dim1 += 1
-        len2, dim2 = input2.size()[1:]
-        if self._use_bias[1]:
-            ones = torch.ones(batch_size, len2, 1)
-            if is_cuda:
-                ones = ones.cuda(device_id)
-            input2 = torch.cat([input2, Variable(ones)], dim=2)
-            dim2 += 1
-        # input1_reshape = (batch* len, dim1)
-        input1_reshaped = input1.contiguous().view(batch_size * len1, dim1)
-        # W_reshaped = (dim1, output_size * dim2)
-        W_reshaped = torch.transpose(self.weight, 1, 2).contiguous().view(dim1, out_size * dim2)
-        # (bn, d) (d, rd) -> (bn, rd) -> (b, nr, d)
-        affine = torch.mm(input1_reshaped, W_reshaped).view(batch_size, len1* out_size, dim2)
-        # (b, nr, d) (b, n, d)T -> (b, nr, n) -> (b, n, r, n)
-        input2_transpose = torch.transpose(input2, 1, 2)
-        biaffine = torch.bmm(affine, input2_transpose).view(batch_size, len1, out_size, len2)
-        # (b, n, r, n) -> (b, n, n, r)
-        biaffine = torch.transpose(biaffine, 2, 3).contiguous()
-        if self._use_bias[2]:
-            biaffine += self.bias.expand_as(biaffine)
-        return biaffine
-
-    def __repr__(self):
-        return self.__class__.__name__ + ' (' \
-                + 'in1_features=' + str(self.in1_features) \
-                + ', in2_features=' + str(self.in2_features) \
-                + ', out_features=' + str(self.out_features) + ')'
 
 class BiLSTMTagger(nn.Module):
 
@@ -220,7 +178,20 @@ class BiLSTMTagger(nn.Module):
         self.Predicate_Proj = nn.Linear(2 * lstm_hidden_dim, lstm_hidden_dim)
         self.W_R = nn.Parameter(torch.ones(lstm_hidden_dim+ 1, self.tagset_size * (lstm_hidden_dim + 1)))
 
-        self.biaffine_classifer = Biaffine(lstm_hidden_dim, lstm_hidden_dim, self.tagset_size)
+        self.mlp_word = MLP(
+            in_features=2 * lstm_hidden_dim,
+            out_features=lstm_hidden_dim,
+            activation=nn.LeakyReLU(0.1),
+            dropout=0.33)
+        nn.init.orthogonal(self.mlp_word.weight)
+        self.mlp_predicate = MLP(
+            in_features=2 * lstm_hidden_dim,
+            out_features=lstm_hidden_dim,
+            activation=nn.LeakyReLU(0.1),
+            dropout=0.33)
+        nn.init.orthogonal(self.mlp_predicate.weight)
+
+
 
         # Init hidden state
         self.hidden = self.init_hidden_spe()
@@ -360,9 +331,9 @@ class BiLSTMTagger(nn.Module):
 
         # B * H
         hidden_states_3 = hidden_states
-        hidden_states = self.Non_Predicate_Proj(hidden_states)
-        predicate_embeds = self.Predicate_Proj(hidden_states_3[np.arange(0, hidden_states_3.size()[0]), target_idx_in])
-        predicate_embeds = predicate_embeds.unsqueeze(1)
+        hidden_states = self.mlp_word(hidden_states)
+        predicate_embeds = self.mlp_predicate(hidden_states_3[np.arange(0, hidden_states_3.size()[0]), target_idx_in])
+
         # T * B * H
         #added_embeds = torch.zeros(hidden_states_3.size()[1], hidden_states_3.size()[0], hidden_states_3.size()[2]).to(device)
         #predicate_embeds = added_embeds + predicate_embeds
@@ -375,7 +346,7 @@ class BiLSTMTagger(nn.Module):
         # B * roles
         # log(local_roles_voc)
         # log(frames)
-        """
+
         bias_one = torch.ones((self.batch_size, len(sentence[0]), 1)).to(device)
         hidden_states_word = torch.cat((hidden_states, Variable(bias_one)), 2)
 
@@ -387,9 +358,7 @@ class BiLSTMTagger(nn.Module):
         hidden_states_predicate = hidden_states_predicate.view(self.batch_size, -1, 1)
         tag_space = torch.bmm(left_part, hidden_states_predicate).view(
             len(sentence[0]) * self.batch_size, -1)
-        """
-        tag_space = self.biaffine_classifer.forward(hidden_states, predicate_embeds).view(
-            len(sentence[0]) * self.batch_size, -1)
+
         SRLprobs = F.softmax(tag_space, dim=1)
 
         # +++++++++++++++++++++++
