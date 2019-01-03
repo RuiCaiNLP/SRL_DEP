23885c461317dd2a0e7e209b1439fa65f2328041
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-01-03 01:10:33 +0000

    +VR

--- nnet/run/srl/run.py
+++ nnet/run/srl/run.py
@@ -172,7 +172,7 @@ class SRLRunner(Runner):
                 #tags.insert(0, '<pad>')
                 dep_seq.append(tags)
             dep_tags = [self.dep_voc.vocalize(p) for p in dep_seq]
-            specific_dep_tags = [self.specific_dep_voc.vocalize(p) for p in specific_dep_labels]
+            specific_dep_tags = [self.dep_voc.vocalize(p) for p in specific_dep_labels]
 
             specific_dep_relations = [[int(r) for r in s ]for s in specific_dep_relations]
 
